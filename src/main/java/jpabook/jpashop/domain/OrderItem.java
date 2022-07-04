95afbd6f7086bc193b004d4234d07b105bd27ad1
jungh8n <jh981113@naver.com>
2022-07-05 01:22:34 +0900

    Web App layer Dev using OrderController with OrderForm

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,50 +1,58 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.naming.Name;
-import javax.persistence.*;
-
 @Entity
 @Table(name = "order_item")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
-    @Id @GeneratedValue
-    @Column(name = "order_item_id")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "order_item_id")
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Order order;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
 
-    private int orderPrice;
-    private int count;
+	private int orderPrice;
+	private int count;
 
-    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
-        OrderItem orderitem = new OrderItem();
-        orderitem.setItem(item);
-        orderitem.setOrderPrice(orderPrice);
-        orderitem.setCount(count);
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+		OrderItem orderitem = new OrderItem();
+		orderitem.setItem(item);
+		orderitem.setOrderPrice(orderPrice);
+		orderitem.setCount(count);
 
-        item.removeStock(count);
-        return orderitem;
-    }
+		item.removeStock(count);
+		return orderitem;
+	}
 
-    public void cancel() {
-        getItem().addStock(count);
-    }
+	public void cancel() {
+		getItem().addStock(count);
+	}
 
-    public int getTotalPrice() {
-        return getOrderPrice() * getCount();
-    }
+	public int getTotalPrice() {
+		return getOrderPrice() * getCount();
+	}
 }
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,5 +1,10 @@
 package jpabook.jpashop.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import jpabook.jpashop.domain.Delivery;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
@@ -8,39 +13,42 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-    private final MemberRepository memberRepository;
-    private final ItemRepository itemRepository;
+	private final OrderRepository orderRepository;
+	private final MemberRepository memberRepository;
+	private final ItemRepository itemRepository;
 
-    @Transactional
-    public Long order(Long memberId, Long itemId, int count){
-        Member member = memberRepository.findOne(memberId);
-        Item item = itemRepository.findOne(itemId);
+	@Transactional
+	public Long order(Long memberId, Long itemId, int count) {
+		Member member = memberRepository.findOne(memberId);
+		Item item = itemRepository.findOne(itemId);
 
-        Delivery delivery = new Delivery();
-        delivery.setAddress(member.getAddress());
+		Delivery delivery = new Delivery();
+		delivery.setAddress(member.getAddress());
 
-        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+		OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
-        Order order = Order.createOrder(member, delivery, orderItem);
+		Order order = Order.createOrder(member, delivery, orderItem);
 
-        orderRepository.save(order);
+		orderRepository.save(order);
 
-        return order.getId();
-    }
+		return order.getId();
+	}
 
-    @Transactional
-    public void cancelOrder(Long orderId){
-        Order order = orderRepository.findOne(orderId);
-        order.cancel();
-    }
+	@Transactional
+	public void cancelOrder(Long orderId) {
+		Order order = orderRepository.findOne(orderId);
+		order.cancel();
+	}
+
+	public List<Order> findOrders(OrderSearch orderSearch) {
+		return orderRepository.findAll(orderSearch);
+	}
 }
