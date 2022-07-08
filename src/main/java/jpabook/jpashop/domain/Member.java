14b8b2bcaf0e36b4269e24d26e0fb64962bf9d44
jungh8n <jh981113@naver.com>
2022-07-08 23:28:20 +0900

    Basic API Dev

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,26 +1,37 @@
 package jpabook.jpashop.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "member")
-@Getter @Setter
+@Getter
+@Setter
 public class Member {
 
-    @Id @GeneratedValue
-    @Column(name = "member_id")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
+	private Long id;
 
-    private String name;
+	@NotEmpty
+	private String name;
 
-    @Embedded
-    private Address address;
+	@Embedded
+	private Address address;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+	@OneToMany(mappedBy = "member")
+	private List<Order> orders = new ArrayList<>();
 }
