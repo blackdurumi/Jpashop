5ec7e40866be672e1f484a3e97d3ffcfdd6d2661
jungh8n <jh981113@naver.com>
2022-07-09 21:52:18 +0900

    order list API Dev. & performance opt.

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,26 +1,35 @@
 package jpabook.jpashop.domain;
 
+import static javax.persistence.FetchType.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-import static javax.persistence.FetchType.*;
-
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Delivery {
 
-    @Id @GeneratedValue
-    @Column(name = "delivery_id")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "delivery_id")
+	private Long id;
 
-    @OneToOne(mappedBy = "delivery", fetch = LAZY)
-    private Order order;
+	@OneToOne(mappedBy = "delivery", fetch = LAZY)
+	private Order order;
 
-    @Embedded
-    private Address address;
+	@Embedded
+	private Address address;
 
-    @Enumerated(EnumType.STRING)
-    private DeliveryStatus status;
+	@Enumerated(EnumType.STRING)
+	private DeliveryStatus status;
 }
